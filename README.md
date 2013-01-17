da0b3bb1224220cf16482cd537fb876f781bd614
Jose F. Romaniello <jfromaniello@gmail.com>
2013-01-17 12:21:11 -0300

    initial

---
+++ README.md
@@ -0,0 +1,27 @@
+Converts files, selection and clipboard content from HTML to JADE using **Aaron Powell**'s http://html2jade.aaron-powell.com/
+
+This is basically a copy of [pavelpachkovskij/sublime-html-to-haml](https://github.com/pavelpachkovskij/sublime-html-to-haml).
+
+## Install
+
+### With Package Control
+
+not yet
+
+### With [Package Decontrol](https://github.com/jfromaniello/Sublime-Package-Decontrol)
+
+~~~
+jfromaniello/sublime-html-to-jade
+~~~
+
+## Shortcuts
+
+* **Convert whole HTML file** `Shift+Alt+F` - creates new file in the same folder using the same name as the source ending with '.html.jade'.
+* **Convert selection** `Shift+Alt+S` - replaces selection of HTML with JADE content.
+* **Convert clipboard content** `Shift+Alt+V` - inserts JADE of converted clipboard HTML content.
+
+### In Command Palette:
+
+* **HTML2Jade: Convert file**
+* **HTML2Jade: Convert selection**
+* **HTML2Jade: Convert clipboard content**
---
+++ html2jade.py
@@ -0,0 +1,63 @@
+import urllib2
+import json
+import sublime
+import sublime_plugin
+
+
+class HtmlToJadeFromFileCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        source = self.view.file_name()
+        if source.endswith(".html"):
+            target = source + '.jade'
+        if target:
+            with open(source, 'r') as f:
+                html = f.read()
+            jade = HTJTools.post_html_return_jade(html)
+            if jade != None:
+                with open(target, 'w') as f:
+                    f.write(jade)
+                self.view.window().open_file(target)
+
+    def is_enabled(self):
+        return True  #return (self.view.file_name().endswith(".html") or self.view.file_name().endswith(".erb"))
+
+
+class HtmlToJadeFromSelectionCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for region in self.view.sel():
+            if not region.empty():
+                html = self.view.substr(region)
+                jade = HTJTools.post_html_return_jade(html)
+                if jade != None:
+                    self.view.replace(edit, region, jade)
+
+    def is_enabled(self):
+        return True #return self.view.file_name().endswith(".jade")
+
+
+class HtmlToJadeFromClipboardCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        html = sublime.get_clipboard()
+        jade = HTJTools.post_html_return_jade(html)
+        if jade != None:
+            for region in self.view.sel():
+                self.view.replace(edit, region, jade)
+
+    def is_enabled(self):
+        return True #return self.view.file_name().endswith(".jade")
+
+
+class HTJTools:
+    @classmethod
+    def post_html_return_jade(self, html):
+        host = 'http://html2jade.aaron-powell.com/convert'
+        data = {'html': html}
+        data_json = json.dumps(data)
+        req = urllib2.Request(host, data_json, {'content-type': 'application/json'})
+        response_stream = urllib2.urlopen(req)
+        result = json.loads(response_stream.read())
+
+        if result["jade"]:
+            return result["jade"][len("html\\n  body\\n"):]
+        else:
+            return None
